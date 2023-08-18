f747bc6fb01d8b645e0f5a97bcb73a1f948a0cfd
Magthylius <56510076+Magthylius@users.noreply.github.com>
2023-08-18 21:49:18 +0800

    chore: add dependencies

--- ProjectMetronome/Source/ProjectMetronome/ProjectMetronome.Build.cs
+++ ProjectMetronome/Source/ProjectMetronome/ProjectMetronome.Build.cs
@@ -7,14 +7,18 @@ public class ProjectMetronome : ModuleRules
 	public ProjectMetronome(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore",
+			"GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
